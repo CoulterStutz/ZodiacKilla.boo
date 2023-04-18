56ddce6003c15193034846e1bb5ad01e617de0fc
coulterminer <coultercash@gmail.com>
2023-04-18 09:16:35 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+let title = document.getElementById("title");
+let message_delay = 200;
 
+window.onload = function(){
+    setTimeout(function(){title.textContent = "ƵodiacKilla"}, message_delay);
+    setTimeout(function(){title.textContent = "ZØdiacKilla"}, message_delay*2);
+    setTimeout(function(){title.textContent = "Zo𝓭iacKilla"}, message_delay*3);
+    setTimeout(function(){title.textContent = "Zod𝓲acKilla"}, message_delay*4);
+    setTimeout(function(){title.textContent = "Zodi𝖆cKilla"}, message_delay*5);
+    setTimeout(function(){title.textContent = "Zodia𝖈Killa"}, message_delay*6);
+    setTimeout(function(){title.textContent = "Zodiac⋊illa"}, message_delay*7);
+    setTimeout(function(){title.textContent = "ZodiacK𝓲lla"}, message_delay*8);
+    setTimeout(function(){title.textContent = "ZodiacKi𝓵la"}, message_delay*9);
+    setTimeout(function(){title.textContent = "ZodiacKil𝓵a"}, message_delay*10);
+    setTimeout(function(){title.textContent = "ZodiacKill𝖆"}, message_delay*11);
+    setTimeout(function(){title.textContent = "ZodiacKilla"}, message_delay*12);
+
+}
